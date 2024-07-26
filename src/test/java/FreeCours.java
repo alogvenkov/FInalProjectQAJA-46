643cd48e3112c0f790d0407947de66cd1ecd34ee
Alogv <Alogvenkov@ya.ru>
2024-07-26 18:42:20 +0700

    1commit

---
+++ src/test/java/FreeCours.java
@@ -0,0 +1,330 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class FreeCours {
+    @Test
+    public void whatProfession() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement testsPRO = driver.findElement(By.xpath("//a[text()='Пройти бесплатно']"));
+            testsPRO.click();
+
+
+            String title = driver.getTitle();
+            Assert.assertEquals(title, "Какая удаленная профессия ваша?");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @Test
+    public void popular() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement testsPRO = driver.findElement(By.xpath("//div[text()='Популярное']"));
+            testsPRO.click();
+
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%9F%D0%BE%D0%BF%D1%83%D0%BB%D1%8F%D1%80%D0%BD%D0%BE%D0%B5&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @Test
+    public void careerGuidance() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement career = driver.findElement(By.xpath("//div[text()='Профориентация']"));
+            career.click();
+
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%9F%D1%80%D0%BE%D1%84%D0%BE%D1%80%D0%B8%D0%B5%D0%BD%D1%82%D0%B0%D1%86%D0%B8%D1%8F&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @Test
+    public void freeSafety() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement safety = driver.findElement(By.xpath("//div[text()='Безопасность']"));
+            safety.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%91%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D1%8C&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void freeAnalitica() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement FAnalitic = driver.findElement(By.xpath("//div[text()='Аналитика']"));
+            FAnalitic.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%90%D0%BD%D0%B0%D0%BB%D0%B8%D1%82%D0%B8%D0%BA%D0%B0&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void freeDataSince() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement DS = driver.findElement(By.xpath("//div[text()='Data Science']"));
+            DS.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=Data+Science&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void freeProg() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement progF = driver.findElement(By.xpath("//div[text()='Разработка']"));
+            progF.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%A0%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void freeManagmetn() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement managF = driver.findElement(By.xpath("//div[text()='Менеджмент в IT']"));
+            managF.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%9C%D0%B5%D0%BD%D0%B5%D0%B4%D0%B6%D0%BC%D0%B5%D0%BD%D1%82+%D0%B2+IT&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void freeTesting() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement testingF = driver.findElement(By.xpath("//div[text()='Тестирование']"));
+            testingF.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%A2%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void freeNeuro() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement neuro = driver.findElement(By.xpath("//div[text()='Нейросети']"));
+            neuro.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%9D%D0%B5%D0%B9%D1%80%D0%BE%D1%81%D0%B5%D1%82%D0%B8&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void freeIi() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement ii = driver.findElement(By.xpath("//div[text()='Искусственный интеллект']"));
+            ii.click();
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_storepartuid[722553837]=%D0%98%D1%81%D0%BA%D1%83%D1%81%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D1%8B%D0%B9+%D0%B8%D0%BD%D1%82%D0%B5%D0%BB%D0%BB%D0%B5%D0%BA%D1%82&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void search() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement Search = driver.findElement(By.xpath("//input[@name='query']"));
+            Search.sendKeys("Тест", Keys.ENTER);
+
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/free-events?tfc_query[722553837]=%D0%A2%D0%B5%D1%81%D1%82&tfc_div=:::");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void Xray() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/free-events");
+            Thread.sleep(2000);
+
+            WebElement phone = driver.findElement(By.xpath("//input[@name='tildaspec-phone-part[]']"));
+            WebElement mail = driver.findElement(By.xpath("(//input[@name='email'])[1]"));
+            phone.sendKeys("9999999999");
+            mail.sendKeys("A@gmail.com", Keys.ENTER);
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/SF_StartPage.java
@@ -0,0 +1,431 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Set;
+
+public class SF_StartPage {
+    @Test
+    public void openStartPage() {
+        String s = System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+
+        driver.get("https://skillfactory.ru/");
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Онлайн-школа Skillfactory — онлайн-обучение востребованным IT-профессиям");
+    }
+
+    @Test
+    public void allCours() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement AllCours = driver.findElement(By.xpath("//a[@class='directions__list-link directions__list-link_green']"));
+            AllCours.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/courses");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void program() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement Program = driver.findElement(By.xpath("(//span[text()='Программирование'])[2]"));
+            Program.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/courses/programmirovanie");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void dataSince() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement datS = driver.findElement(By.xpath("//a[@href='/courses/data-science']"));
+            datS.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/courses/data-science");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void dataAnalytics() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement datA = driver.findElement(By.xpath("//a[@href='/courses/analitika-dannyh']"));
+            datA.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/courses/analitika-dannyh");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void testing() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement tests = driver.findElement(By.xpath("//a[@href='/courses/testirovanie']"));
+            tests.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/courses/testirovanie");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void higherEducation() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement HE = driver.findElement(By.xpath("(//span[text()='Высшее образование'])[2]"));
+            HE.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://new.skillfactory.ru/vyssheye-obrazovaniye");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void design() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement Design = driver.findElement(By.xpath("(//span[text()='Дизайн'])[2]"));
+            Design.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/courses/design");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void managment() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement Manag = driver.findElement(By.xpath("(//span[text()='Менеджмент'])[2]"));
+            Manag.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://skillfactory.ru/courses/management-i-upravlenie");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void contacts() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement Manag = driver.findElement(By.xpath("//a[text()='контакты']"));
+            Manag.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String title = driver.getTitle();
+            Assert.assertEquals(title, "Контакты школы по работе с данными Skillfactory");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void free() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement Free = driver.findElement(By.xpath("//a[text()='БЕСПЛАТНО']"));
+            Free.click();
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+            for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String title = driver.getTitle();
+            Assert.assertEquals(title, "Бесплатные курсы, онлайн обучение профессиям с получением сертификата, семинары и вебинары бесплатно от Skillfactory");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Test
+    public void pointingAndClick() {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        try {
+            driver.get("https://skillfactory.ru/");
+            Thread.sleep(2000);
+
+            String window1 = driver.getWindowHandle();
+
+            WebElement dropDown = driver.findElement(By.xpath("//a[@class='main__burger t978__tm-link']"));
+            WebElement forum = driver.findElement(By.xpath("//span[text()='БЛОГ']"));
+            dropDown.click();
+            Actions actions = new Actions(driver);
+            actions.moveToElement(forum).click().build().perform();
+
+
+            Set<String> currentWindows = driver.getWindowHandles();
+
+            String window2 = null;
+           for (String window : currentWindows) {
+                if (!window.equals((window1))) {
+                    window2 = window;
+                    break;
+                }
+            }
+            driver.switchTo().window(window2);
+            String url = driver.getCurrentUrl();
+            Assert.assertEquals(url, "https://blog.skillfactory.ru/");
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
